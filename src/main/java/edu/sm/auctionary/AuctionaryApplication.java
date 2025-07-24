dd4782d4584df49531032d4d730253bcab63dddc
moonpoison <moonpoison@kakao.com>
2025-07-24 15:19:33 +0900

    first commit

---
+++ src/main/java/edu/sm/auctionary/AuctionaryApplication.java
@@ -0,0 +1,13 @@
+package edu.sm.auctionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/sm/auctionary/AuctionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sm.auctionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
